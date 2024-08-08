0f859d7929d1ed49409b83bc4b28148bd4db7c24
Mia Fusco-Robinson <miafusco3@gmail.com>
2024-08-08 15:22:07 -0500

    Update exercises.py

--- data-and-variables/exercises/exercises.py
+++ data-and-variables/exercises/exercises.py
@@ -1,6 +1,11 @@
-# 1. Declare and assign the variables here:
-
-# 2. Use print() to print the 'type' each variable. Print one item per line.
+shuttle name = 'Determination'
+shuttle_speed_mph = 17500
+distance_to_mars_km = 38400
+MILES_PER_KM = 0.621
+print(type(shuttle_name))
+Print(type(shuttle_speed_mph))
+print(type(distance_to_mars_km))
+print(type(MILES_PER_KM))
 
 # Code your solution to exercises 3 and 4 here:
 
