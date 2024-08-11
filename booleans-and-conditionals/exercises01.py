4d618ad81816b48e68a3961265678c6d385861f5
Mia Fusco <miafusco3@gmail.com>
2024-08-11 14:34:02 -0500

    Week 13

--- booleans-and-conditionals/exercises01.py
+++ booleans-and-conditionals/exercises01.py
@@ -1,4 +1,10 @@
 # Assign the variables for exercise 1 here:
+engine_indicator_light = "red blinking"
+space_suits_on = True
+shuttle_cabin_ready = True
+crew_status = space_suits_on and shuttle_cabin_ready
+computer_status_code = 200
+shuttle_speed = 15000
 
 
 
