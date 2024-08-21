5d1b27b1fb1733abc462c35824a895682fa89c09
Mia Fusco <miafusco3@gmail.com>
2024-08-20 19:40:54 -0500

    Week 13

--- loops/exercises/for-loop-exercises.py
+++ loops/exercises/for-loop-exercises.py
@@ -1,7 +1,17 @@
 # Exercise #1: Construct for loops that accomplish the following tasks:
     # a. Print the numbers 0 - 20, one number per line.
+for num in range(21):
+    print(num)
+
     # b. Print only the ODD values from 3 - 29, one number per line.
+    for num in range(3,29):
+        print(num)
+    if num%2 != 0:
+        print(num)
+
     # c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
+    for num in range(12,-14):
+        print(num)
     # d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers).
 
 
