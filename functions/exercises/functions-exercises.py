8e69e801edf8420c6b5735b7e1b6a7d7247d69ee
Mia Fusco <miafusco3@gmail.com>
2024-09-03 21:27:11 -0500

    Studio 18

--- functions/exercises/functions-exercises.py
+++ functions/exercises/functions-exercises.py
@@ -31,12 +31,12 @@ def make_square(size):
 print(make_square(5))  
 
 # Part 2 A -- Make a Stairs
-def make_downward_stairs(height):
-    stairs = ""
-    for i in range(height):
-        stairs += (make_line(i +1) + "\n")
-    return stairs
-print(make_downward_stairs(5))
+def make_downward_stairs(height): #define function
+    stairs = ""                   # initialize empty string variable called stairs
+    for i in range(height):       # for each iteration(I) of height within the range
+        stairs += (make_line(i +1) + "\n")    #update the stairs variable with the value returned by calling the make_line function, adding 1 to it, and a line break
+    return stairs                             # return the result
+print(make_downward_stairs(5))                # call the function using 5 as the argument and print the result 
 
 # Part 2 B -- Make Space-Line 
 def make_space_line(numSpaces, numChars):
@@ -63,3 +63,25 @@ def make_diamond(height):
         diamond += triangle[i]
     return diamond
 print(make_diamond(5))
+
+
+# Class Demo 9/3
+def square_number(x):
+    result = x * x 
+    return result
+result = square_number(4)
+print(result)
+
+def add_numbers_together(num):  #function body-function name and parameter
+    total = 0                   #function body-initializes the variable called total and sets it to zero
+    while num < 100:            #function body-begins a while loop that continues until the value of num is 100 or greater
+        total += num            #function body-repeatedly adds the current value of num to the total
+        num += 1                #function body-after each addition, value of num increases by 1
+    return total                #the result that the funtionc returns
+result = add_numbers_together(4)
+print(result)
+def add_numbers(x,y):
+    result = x + y
+    return result
+result = add_numbers(4, 5)
+print(result)
--- functions/studio/functions-studio.py
+++ functions/studio/functions-studio.py
@@ -1,21 +1,50 @@
 # We want to COMPLETELY reverse a list by flipping the order of the entries AND flipping the order of characters in each element.
 
 # a) Define a 'reverse_characters' function. Give it one parameter, which will be the string to reverse.
+def reversed_characters(string):   #function name and parameter
+    string_list = list(string)
+    string_list.reverse()
+    reversed_string = "".join(string_list)
+    return reversed_string
+    return string[::-1]             #the result that the function returns
+# print(reversed_characters('apple'))  #this is what outputs here
+# print(reversed_characters('mia'))
 # b) Within the function, use the 'list' function to split a string into a list of individual characters
-# c) 'reverse' your new list.
+string_list = ['apple', 'LC101', 'Capitalized Letters', 'I love the smell of code in the morning.']
+print(string_list)
+# # c) 'reverse' your new list.
+string_list.reverse()
+print(string_list)
 # d) Use 'join' to create the reversed string and return that string from the function.
-# e) Create a variable of type string to test your new function. # f) Use 'print(reverse_characters(my_variable_name))'; to call the function and verify that it correctly reverses the characters in the string.
+# e) Create a variable of type string to test your new function.
+# f) Use 'print(reverse_characters(my_variable_name))'; to call the function and verify that it correctly reverses the characters in the string.
+object = 'water bottle'
+print(type(object))
+print(reversed_characters(object))
 # g) Use method chaining to reduce the lines of code within the function.
-
-
 
 # 2) The 'split' method does not work on numbers, but we want the function to return a number with all the digits reversed (e.g. 1234 converts to 4321 and NOT the string "4321")
 # a) Add an if statement to your reverse_characters function to check the typeof the parameter.
+def reversed_characters(string):
+    if type(str):
+        return string.reverse()
+    elif type(int):
+        reversed_string = 
+        
 # b - d) If type is ‘string’, return the reversed string as before. If type is ‘number’, convert the parameter to a string, reverse the characters, then convert it back into a number. Return the reversed number.
 # e) Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next steps.
 
 # 3) Create a new function with one parameter, which is the list we want to change. The function should:
 # a) Define and initialize an empty list.
+def new_list(list_test1):
+    this_list = []
+    for item in list_test1:
+        reversed_item = list_test1.reverse()
+    
+    return this_list
+    print(this_list)
+
+
 # b) Loop through the old list.
 # c) For each element in the old list, call reverse_characters to flip the characters or digits.
 # d) Add the reversed string (or number) to the list defined in part ‘a’.
